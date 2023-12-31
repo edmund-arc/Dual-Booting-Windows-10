76dbed5a013abb14f8367bff12d8463e4023f6b9
edmund-arc <154868349+edmund-arc@users.noreply.github.com>
2023-12-31 21:29:57 +0800

    Update verifySuccess.py
    Add step to verify the dual boot working.

--- verifySuccess.py
+++ verifySuccess.py
@@ -2,3 +2,9 @@
 
 # The verifying steps is as follows:
 # 1. After cloning both partitions with respective OS (both Windows 10), run a hello world!
+# 2. Suggest step to clone as follows:
+# 3. Clone in the Tech Refresh OS and program
+# 4. Add a new partition to the current
+# 5. Restart and clone in the old OS + program to the new partition
+# 6. Boot up the new program and verify any issues
+# 7. Reboot and change boot partition and run the old OS + program and verify any issue.
