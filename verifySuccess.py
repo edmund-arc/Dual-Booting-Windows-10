1a3399ae72744a612dbdfec312dc5ead66808687
edmund-arc <154868349+edmund-arc@users.noreply.github.com>
2023-12-31 21:25:01 +0800

    Create verifySuccess.py

---
+++ verifySuccess.py
@@ -0,0 +1,4 @@
+# The purpose of the this pythin script is to verify objective of this project, that is, to verify dual booting is successful.
+
+# The verifying steps is as follows:
+# 1. After cloning both partitions with respective OS (both Windows 10), run a hello world!
